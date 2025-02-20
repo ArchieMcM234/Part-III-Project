b8db093866cb808f1c1bbc658a007369ffe46087
ArchieMcM234 <archiemcm234@gmail.com>
2025-02-20 16:11:16 +0000

    first attempt at ccd - nicely formatted code

--- classes/quantum_classes.py
+++ classes/quantum_classes.py
@@ -45,7 +45,15 @@ def calculate_z_expectation(states):
         z_expectation.append(state.conj() @ sigma_z @ state)
     return np.array(z_expectation)
 
-from functools import reduce
+def calculate_expectations(states, obervable):
+    """
+    Takes array of states and returns the expecation of pauli z on them - only single qubit
+    """
+    expectations = []
+    for state in states:
+        expectations.append(state.conj() @ obervable @ state)
+    return np.array(expectations)
+
 
 def kron_multiple_arrays(array_list):
     """
@@ -84,7 +92,7 @@ class Quantum_Hamiltonian:
         H = np.zeros((2**self.num_qubits, 2**self.num_qubits), dtype=complex)
 
         for a in range(self.num_qubits):
-            single_hamiltonian = hbar*(1/2) * np.array([
+            single_hamiltonian = (1/2) * np.array([
             [-(self.natural_omegas[a] - driving_omega), self.rabi_omega],
             [self.rabi_omega, self.natural_omegas[a] - driving_omega]])
 
@@ -93,13 +101,35 @@ class Quantum_Hamiltonian:
 
         return H
 
-    def ccd_rwa(self):
-        # TODO
-        pass
+    def ccd_rwa(self, phi_0, epsilon_m, phase_freq, theta_m, t, driving_freq):
+        """
+        Time-independent Hamiltonian in the rotating wave approximation (RWA) for CCD.
+        """
+        driving_omega = 2 * np.pi * driving_freq
+        phase_omega = 2 * np.pi * phase_freq
 
-    def ccd_non_rwa(self):
-        # TODO
-        pass
+        delta = self.natural_omegas[0] - driving_omega
+        cos_phi_0 = np.cos(phi_0)
+        sin_phi_0 = np.sin(phi_0)
+        cos_phase = epsilon_m * (phase_omega / self.rabi_omega) * np.cos(phase_omega * t - theta_m)
+        # print('delta', delta,'cosphi0', cos_phi_0, 'sin_phio', sin_phi_0, 'cosphase', cos_phase)
+        H = - (delta / 2) * sigma_z + (self.rabi_omega / 2) * (cos_phi_0 * sigma_x + sin_phi_0 * sigma_y) +cos_phase * sigma_z
+            
+        return H
+
+    def ccd_non_rwa(self, phi_0, epsilon_m, phase_freq, theta_m, t, driving_freq):
+        """
+        Time-dependent Hamiltonian in the non-rotating wave approximation (non-RWA) for CCD.
+        """
+        driving_omega = 2 * np.pi * driving_freq
+        phase_omega = 2 * np.pi * phase_freq
+
+        delta = self.natural_omegas[0] - driving_omega # this definition is the opposite
+        cos_term = np.cos(driving_omega * t + phi_0 - (2 * epsilon_m / self.rabi_omega) * np.sin(phase_omega * t - theta_m))
+        
+        H = (delta / 2) * sigma_z + self.rabi_omega * cos_term * sigma_x
+            
+        return H
 
 
 # Im gonna think that this is permanent stuff - may be better to split up but press on
@@ -124,7 +154,7 @@ class Quantum_System:
         basis_state[0] = 1.0  # Ground state |0...0>
         return basis_state
     
-    def evolve_state(self, initial_state, time, num_points, driving_freq, ham_type="rwa", **kwargs):
+    def evolve_state(self, initial_state, time, num_points, ham_type="rwa", **kwargs):
         """
         Evolve the system using the time-dependent Schrödinger equation.
         
@@ -140,12 +170,14 @@ class Quantum_System:
 
         def tdse(t, psi):
             if ham_type == "rwa":
-                H = self.hamiltonian.rwa(driving_freq*2*np.pi)
+                H = self.hamiltonian.rwa(kwargs['driving_freq']*2*np.pi)
             elif ham_type == "idle":
                 H = self.hamiltonian.idle()
+            elif ham_type == "ccd_rwa":
+                H = self.hamiltonian.ccd_rwa(**kwargs, t=t)
             else:
                 raise ValueError("Unknown Hamiltonian type.")
-            return -1j / hbar * (H @ psi)
+            return -1j * (H @ psi)
         
 
         t_span = (0, time)  # From t=0 to t=10
@@ -158,7 +190,7 @@ class Quantum_System:
 
         return sol.t, sol.y.T  # Return the full evolution for analysis
 
-    def find_total_hamiltonians(self, time, num_points, driving_freq, ham_type="rwa", **kwargs):
+    def find_total_hamiltonians(self, time, num_points, ham_type="rwa", **kwargs):
 
         total_hamiltonian = []
         
@@ -166,16 +198,14 @@ class Quantum_System:
             initial_state = np.zeros(2**self.num_qubits, dtype=complex)
             initial_state[initial_state_index] = 1  # Initialize basis state
             
-            t, y = self.evolve_state(initial_state, time, num_points, driving_freq, ham_type, **kwargs)
+            t, y = self.evolve_state(initial_state, time, num_points, ham_type=ham_type, **kwargs)
             
             # Now we stack the evolved states for all basis states at each time step
             total_hamiltonian.append(y)
 
         # Stack all the evolved states for each basis state along the second axis
         # Result will be (num_points, 2**num_qubits)
-        return t, np.stack(total_hamiltonian, axis=1) 
-
-
+        return t, np.stack(total_hamiltonian, axis=1)
 
     
     def expectation(self, observable):
@@ -190,13 +220,86 @@ class Quantum_System:
         """
         return np.real(self.state.conj().T @ observable @ self.state)
 
+    def rotate_state(self, state, theta, axis):
+        """
+        Rotate the frame of the quantum state by an angle theta around a given axis.
+        
+        Parameters:
+        - theta (float): Angle by which to rotate the frame.
+        - axis (np.array): Axis around which to rotate (must be a 3-element array).
+        
+        Returns:
+        - np.array: Rotated state.
+        """
+        # Normalize the axis vector
+        axis = axis / np.linalg.norm(axis)
+        
+        # Pauli matrices
+        pauli_matrices = [sigma_x, sigma_y, sigma_z]
+        
+        # Compute the rotation matrix
+        rot_mat = np.cos(theta / 2) * np.eye(2) - 1j * np.sin(theta / 2) * sum(axis[i] * pauli_matrices[i] for i in range(3))
+        
+        return rot_mat @ state
+    def rotate_system(self, t, y, theta_func, axis_func):
+        """
+        Rotate the frame of the quantum system by an angle theta around a given axis.
+        
+        Parameters:
+        - t (array): Time points at which the solution is evaluated.
+        - y (array): Solution array with shape (num_points, 2**num_qubits).
+        - theta_func (function): Function of time that returns the rotation angle theta.
+        - axis_func (function): Function of time that returns the rotation axis (must be a 3-element array).
+        
+        Returns:
+        - array: Rotated solution array with the same shape as y.
+        """
+        rotated_solution = np.zeros_like(y, dtype=complex)
+        
+        for i in range(len(t)):
+            theta = theta_func(t[i])
+            axis = axis_func(t[i])
+            rotated_solution[i] = self.rotate_state(y[i], theta, axis)
+        
+        return rotated_solution
+    
 
-    def rotate_frame(self):
-        # TODO
-        pass
+    def visualise_solution(self, t, y):
+        """
+        Visualize the solution of the quantum system on a Bloch sphere.
+        
+        Parameters:
+        - t (array): Time points at which the solution is evaluated.
+        - y (array): Solution array with shape (num_points, 2**num_qubits).
+        """
+        # Calculate expectations
+        x_expectation = calculate_expectations(y, sigma_x)
+        y_expectation = calculate_expectations(y, sigma_y)
+        z_expectation = calculate_expectations(y, sigma_z)
 
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
+        ax.set_zlim([-1, 1])
 
+        # Function to update the Bloch vector
+        def update(frame):
+            ax.cla()  # Clear the previous frame
+            ax.set_title("Animated Bloch Vector")
+            ax.set_xlim([-1, 1])
+            ax.set_ylim([-1, 1])
+            ax.set_zlim([-1, 1])
 
+            # Plot the Bloch vector
+            ax.quiver(0, 0, 0, x_expectation[frame], y_expectation[frame], z_expectation[frame], 
+                      color='r', arrow_length_ratio=0.1)
+
+        # Create the animation
+        ani = FuncAnimation(fig, update, frames=np.arange(0, len(t), 1), interval=50)
+
+        plt.show()
+        # ani.save("Bloch_Vector.gif", writer='pillow', fps=30)
 
 
 
